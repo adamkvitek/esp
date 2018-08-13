6463ec8a6a8f019822f9176e39927112194b7e89
Adam Kvítek <adamkvitek@protonmail.com>
2018-08-13 16:45:35 +0200

    Adding oled code to main.py

---
+++ oled/main.py
@@ -0,0 +1,12 @@
+from ssd1306 import SSD1306_I2C
+from machine import I2C, Pin
+
+i2c = I2C(scl=Pin(18), sda=Pin(19), freq=100000)
+
+d = ssd1306.SSD1306_I2C(128, 64, i2c)
+
+d.text("ahoj", 10, 10)
+d.show()
+
+# To delete the text for possible additions of a new text:
+# d.fill(0)
